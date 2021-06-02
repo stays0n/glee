26b1951d34a6ba8133e7d5f751908c4f36510eb9
stays0n <risis666@gmail.com>
2021-06-02 13:06:50 +0300

    made edits. v1.2

--- app/js/main.js
+++ app/js/main.js
@@ -1,12 +1,22 @@
 $(function () {
-  // лишний отступ у функции встроенный плагин добавляет постоянно, постараюсь внимательнее(
 
   const headerMenu = document.querySelector('.header__menu'),
     headerTools = document.querySelector('.header__tools'),
-    headerClose = document.querySelector('.header__close')
+    headerClose = document.querySelector('.header__close'),
+    header = document.querySelector('.header')
 
-  function showHideTools() {
-    if (window.innerWidth <= 568 || window.pageYOffset > 55) {
+  function togglePaddindOnToolsFixed() {
+    if (window.innerWidth < 569 && window.pageYOffset > 54) {
+      header.style.paddingBottom = 85 + 'px'
+      toggleTools()
+
+    } else if (window.innerWidth > 569 || window.pageYOffset <= 54) {
+      header.style.paddingBottom = ''
+    }
+  }
+
+  function toggleTools() {
+    if (window.innerWidth <= 823 || window.pageYOffset > 55) {
       headerTools.classList.add('header__tools--fixed')
 
     } else {
@@ -14,11 +24,11 @@ $(function () {
     }
   }
 
-  function showHideHeader() {
+  function toggleHeader() {
     if (window.pageYOffset > 95) {
       headerMenu.classList.add('header__menu--active')
 
-    } else if (window.pageYOffset <= 95) {
+    } else {
       headerMenu.classList.remove('header__menu--active')
     }
   }
@@ -28,39 +38,38 @@ $(function () {
       headerMenu.classList.remove('header__menu--active')
       return
     }
-    showHideHeader()
+    toggleHeader()
   })
 
   document.addEventListener('scroll', () => {
-    if (window.innerWidth > 568 || window.pageYOffset < 55) {
+    if (window.innerWidth > 823 || window.pageYOffset < 55) {
       headerTools.classList.remove('header__tools--fixed')
+      togglePaddindOnToolsFixed()
       return
     }
-    showHideTools()
+    togglePaddindOnToolsFixed()
+    toggleTools()
   })
 
   const menuBurger = document.querySelector('.header__tool--burger'),
     bodyTag = document.body;
   let menuToggleFlag = false
 
-  const removeActiveStyles = () => {
-    headerMenu.classList.remove('header--burger-active')
-    headerClose.classList.remove('header__close--active')
-    bodyTag.style.overflow = 'visible'
-    menuToggleFlag = false
-  }
-
-  menuBurger.onclick = function toggleMenu() {
-    if (menuToggleFlag === false) {
+  const toggleActiveStyles = isRemove => {
+    if (isRemove) {
+      headerMenu.classList.remove('header--burger-active')
+      headerClose.classList.remove('header__close--active')
+      bodyTag.style.overflow = ''
+      menuToggleFlag = false
+    } else {
       headerMenu.classList.add('header--burger-active')
       headerClose.classList.add('header__close--active')
       bodyTag.style.overflow = 'hidden'
       menuToggleFlag = true
-
-    } else if (menuToggleFlag === true) {
-      removeActiveStyles()
     }
   }
+
+  menuBurger.onclick = () => menuToggleFlag ? toggleActiveStyles(true) : toggleActiveStyles(false)
 
   document.addEventListener('click', outsideEvtListener);
 
@@ -68,7 +77,7 @@ $(function () {
     if (evt.target === menuBurger || menuBurger.contains(evt.target)) {
       return
     }
-    removeActiveStyles()
+    toggleActiveStyles(true)
   }
 
   const formInput = document.querySelector('.form__input'),
@@ -84,6 +93,21 @@ $(function () {
     }
   })
 
+  // shop-catalog toggle
+  $('.shop-catalog__btn').on('click', function () {
+    $('.shop-catalog__btn').removeClass('shop-catalog__btn--active')
+    $(this).addClass('shop-catalog__btn--active')
+  });
+
+  $('.button-list').on('click', function () {
+    $('.shop-catalog__grid').addClass('shop-catalog__grid--list')
+  });
+
+  $('.button-grid').on('click', function () {
+    $('.shop-catalog__grid').removeClass('shop-catalog__grid--list')
+  });
+
+  // slickslider
   $('.banner__slider').slick({
     arrows: false,
     dots: true,
@@ -93,7 +117,7 @@ $(function () {
     arrows: false,
     slidesToShow: 5,
     slidesToScroll: 1,
-    autoplay: true,
+    // autoplay: true,
     autoplaySpeed: 1000,
     centerMode: true,
     centerPadding: '-100px',
@@ -120,6 +144,41 @@ $(function () {
     ]
   });
 
+  // rangeslider
+  $('.filters-price__input').ionRangeSlider({
+    type: 'double',
+    prefix: '$',
+    hide_min_max: true,
+    hide_from_to: true,
+    prettify_enabled: true,
+    prettify_separator: ".",
+
+    onStart: function (data) {
+      $('.filters-price__from').text('$' + data.from.toFixed(2));
+      $('.filters-price__to').text('$' + data.to.toFixed(2));
+    },
+    onChange: function (data) {
+      $('.filters-price__from').text('$' + data.from.toFixed(2));
+      $('.filters-price__to').text('$' + data.to.toFixed(2));
+    },
+  });
+
+  // rateyo
+  $(".star").rateYo({
+    starWidth: '18px',
+    normalFill: '#d6d6d6',
+    ratedFill: '#ffcc00',
+    readOnly: true,
+    spacing: '13px',
+    starSvg: '<svg xmlns="http://www.w3.org/2000/svg" width="13px" height="13px" viewBox="0 0 576 512">' +
+      '<path d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z"></path>' +
+      '</svg>'
+  });
+
+  // formstyler
+  $('.select-style').styler();
+
+  // mixitup
   const containerEl1 = document.querySelector('[data-ref="container-1"]'),
     containerEl2 = document.querySelector('[data-ref="container-2"]'),
     config = {
